b16905a44e585eb7b3476ee1d79048a545f538c8
bp0 <ijleoni@gmail.com>
2020-10-06 20:40:51 -0300

    extra options

--- install_software.sh
+++ install_software.sh
@@ -7,10 +7,25 @@ source /root/.bashrc
 
 #my tools
 go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+go get -u -v github.com/michenriksen/aquatone
 go get -u -v github.com/dwisiswant0/unew
+go get -u -v github.com/tomnomnom/unfurl
 go get -u -v github.com/tomnomnom/anew
+go get -u -v github.com/mgumz/knut
 go get -u -v github.com/ffuf/ffuf
 go get -u -v github.com/lc/gau
 
 go get -u -v github.com/bp0lr/dalfox
 go get -u -v github.com/bp0lr/wurl
+
+
+git clone https://github.com/maurosoria/dirsearch.git /opt/bp0/dirsearch
+git clone https://github.com/bp0lr/wordlists.git /opt/bp0/wordlists
+
+###########################################################
+# recon
+###########################################################
+git clone https://github.com/bp0lr/reconme.git /opt/bp0/reconme
+
+cd /opt/bp0/reconme
+knut /reconme:/opt/bp0/test/ > /dev/null 2>&1 &
