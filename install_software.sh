bb56b404373e92196048294827509ce773c853f2
bp0 <ijleoni@gmail.com>
2020-10-06 21:22:24 -0300

    chaos client

--- install_software.sh
+++ install_software.sh
@@ -1,11 +1,17 @@
-apt install -y wget git
+cd /root
+apt install -y wget git python3 python3-pip
 
 # this one liner will install goland
 # ref: https://github.com/canha/golang-tools-install-script
 wget -q -O - https://git.io/vQhTU | bash
 source /root/.bashrc
+rm -rf /root/go
+
+echo "alias ls='ls --color=auto'" >> ~/.bashrc 
+source /root/keys.txt
 
 #my tools
+go get -u -v github.com/projectdiscovery/chaos-client/cmd/chaos
 go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 go get -u -v github.com/michenriksen/aquatone
 go get -u -v github.com/dwisiswant0/unew
@@ -18,9 +24,12 @@ go get -u -v github.com/lc/gau
 go get -u -v github.com/bp0lr/dalfox
 go get -u -v github.com/bp0lr/wurl
 
-
 git clone https://github.com/maurosoria/dirsearch.git /opt/bp0/dirsearch
 git clone https://github.com/bp0lr/wordlists.git /opt/bp0/wordlists
+
+git clone https://github.com/devanshbatham/ParamSpider /opt/bp0/ParamSpider
+cd /opt/bp0/ParamSpider
+pip3 install -r requirements.txt
 
 ###########################################################
 # recon
